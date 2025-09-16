b495f6dcff491707b15ba2f4e1766046f14c0a26
Yashraj <yashrajdahiya0036@gmail.com>
2025-09-16 18:29:17 +0530

    added basic commands to git.js

---
+++ git.js
@@ -0,0 +1,5 @@
+// git add ./   to add all files to stage
+// git add filename to add a single file to stage
+// git commit -m 'add the commit message'
+// git staus to track the status of files 
+// git log shows the previous commits 
